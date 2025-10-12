3e2b1b40c5b2b35e94db68992cf24ee0255cddd7
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2025-10-12 09:00:55 +0300

    Update GitHubRelease_Test.bat

--- 04-github-release-download/GitHubRelease_Test.bat
+++ 04-github-release-download/GitHubRelease_Test.bat
@@ -2,6 +2,10 @@
 
 rem This is a test file for GitHubRelease.bat
 
+setlocal
+
+rem Libjpeg-Turbo
+
 set "COMMON_NAME=libjpeg-turbo"
 set "REPO_NAME=libjpeg-turbo/libjpeg-turbo"
 REM - Direct download
@@ -15,4 +19,25 @@ set "SPECIFIC_NAME="
 
 call "GitHubRelease.bat" & set "EXIT_STATUS=%ERRORLEVEL%"
 
+endlocal
+
+
+setlocal
+
+rem Micromamba
+
+set "COMMON_NAME=Micromamba"
+set "REPO_NAME=mamba-org/micromamba-releases"
+REM - Direct download
+set "ASSET_URL_SUFFIX="
+set "RELEASE_URL_SUFFIX=micromamba-win-64.exe"
+REM - File extension in URL.
+set "DOWNLOAD_EXT="
+set "CANONICAL_NAME=micromamba.exe"
+rem Use old file name from URL
+set "SPECIFIC_NAME="
+
+call "GitHubRelease.bat" & set "EXIT_STATUS=%ERRORLEVEL%"
+
 endlocal & exit /b %EXIT_STATUS%
+
